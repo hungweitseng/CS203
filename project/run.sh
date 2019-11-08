2ac0134bdcf56516adac0caeca2787eda9e8c097
hunweitseng <h1tseng@cs.ucsd.edu>
2019-11-07 21:57:26 -0800

    Remove project

--- project/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd baseline; time ./mcf ../data/ref/input/inp.in >& /dev/zero; cd ..
-cd src; time ./mcf ../data/ref/input/inp.in >& /dev/zero ; cd ..
-diff ./baseline/mcf.out ./src/mcf.out
