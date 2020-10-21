ec86db9e646487c103e2f799563c1938db9ca1cf
hunweitseng <h1tseng@cs.ucsd.edu>
2020-10-21 13:41:19 -0700

    Remove project

--- project_2020/baseline.h
+++
@@ -1,1 +0,0 @@
-void baseline_threaded_vector_blockmm(double **a, double **b, double **c, int ARRAY_SIZE, int n, int number_of_threads);
--- project_2020/myblockmm.h
+++
@@ -1,4 +0,0 @@
-extern char name[128];
-extern char SID[128];
-
-void my_threaded_vector_blockmm(double **a, double **b, double **c, int n, int ARRAY_SIZE, int number_of_threads);
